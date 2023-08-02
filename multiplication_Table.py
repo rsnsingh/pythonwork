2162ee77f37a6d616ad39c03f09af115e0272d4d
Ravinder Singh <42645016+rsnsingh@users.noreply.github.com>
2023-08-02 15:18:34 +0530

    Add files via upload

---
+++ multiplication_Table.py
@@ -0,0 +1,13 @@
+for x in range(1,10):
+   print("  ", x, end = '')
+   print()
+for row in range(1, 10):
+    for col in range(1, 10):
+        num = row * col
+        if num < 10:
+            empty = "  "
+        else:
+            if num < 100:
+                empty  = " "
+        print(empty, num, end = '')
+    print()
